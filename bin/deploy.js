2ad39427eb0c6b9bd77fb067bf8bd512169bbf9b
zhouzh1 <zzhouzhii@gmail.com>
2019-05-19 23:03:11 +0800

    fix

--- bin/deploy.js
+++ bin/deploy.js
@@ -3,7 +3,7 @@
  * @Author: zhouzh1 
  * @Date: 2019-05-18 23:51:11 
  * @Last Modified by: zhouzh1
- * @Last Modified time: 2019-05-19 17:44:30
+ * @Last Modified time: 2019-05-19 22:45:05
  */
 
 const logger = require('../lib/logger');
@@ -17,7 +17,6 @@ const checkroot = require('../lib/checkroot');
 checkroot.check();
 
 const { spawn } = require('child_process');
-const { EOL } = require('os');
 
 // options for spawn
 const options = {
@@ -36,27 +35,27 @@ function runner() {
 	// git commit -m `${time}`
 	// git push origin source:source
 	// git subtree push --prefix public origin master
-	logger.info(`${EOL}[+] git pull origin source:source`);
+	logger.info('git pull origin source:source');
 	const pull = spawn('git', ['pull', 'origin', 'source:source'], options);
 	pull.on('close', function (code) {
 		if (code === 0) {
-			logger.info(`${EOL}git status`);
+			logger.info('git status');
 			const status = spawn('git', ['status'], options);
 			status.on('close', function (code) {
 				if (code === 0) {
-					logger.info(`${EOL}git add -A`);
+					logger.info('git add -A');
 					const add = spawn('git', ['add', '-A'], options);
 					add.on('close', function (code) {
 						if (code === 0) {
-							logger.info(`${EOL}git commit`);
+							logger.info('git commit');
 							const commit = spawn('git', ['commit', '-m', (new Date()).toLocaleString()], options);
 							commit.on('close', function (code) {
 								if (code === 0) {
-									logger.info(`${EOL}git push origin source:source`);
+									logger.info('git push origin source:source');
 									const sourceBranch = spawn('git', ['push', 'origin', 'source:source'], options);
 									sourceBranch.on('close', function (code) {
 										if (code === 0) {
-											logger.info(`${EOL}git subtree push --prefix public origin master`);
+											logger.info('git subtree push --prefix public origin master');
 											spawn('git', ['subtree', 'push', '--prefix', 'public', 'origin', 'master'], options);
 										}
 									});
